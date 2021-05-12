70137924dab4c3c3965748ba625f2d8d206b825d
Flemming Knøss Hansen <fha@kmd.dk>
2021-05-12 22:23:43 +0200

    Leg

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,13 +1,24 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, TextInput } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
+  const [ text, setText ] = useState('')
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Fars fede dillermandse</Text>
+      <Text style={styles.subTitle}>Dette er bare en test</Text>
+      <TextInput
+        style={{height: 40, width: "80%"}}
+        placeholder="When do you want to set status?" 
+        onChangeText={text => setText(text)}
+        defaultValue={text}
+      />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
@@ -23,6 +34,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  subTitle: {
+    fontSize: 20,
+    fontWeight: 'normal',
   },
   separator: {
     marginVertical: 30,
